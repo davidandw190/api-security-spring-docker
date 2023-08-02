0948b2216c7a7db9dba8d54fdce98a621762efc3
Andrei-David Nan <nandavid190@gmail.com>
2023-08-02 20:03:11 +0300

    v0.10 - Setup Security Configuration and User Details Service implementation;

---
+++ src/main/java/com/davidandw190/apisecurity/service/implementation/UserDetailsServiceImpl.java
@@ -0,0 +1,49 @@
+package com.davidandw190.apisecurity.service.implementation;
+
+import com.davidandw190.apisecurity.model.Account;
+import com.davidandw190.apisecurity.service.AccountService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final AccountService accountService;
+
+    public UserDetailsServiceImpl(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountService.findByUsername(username);
+
+        if (account == null) {
+            throw new UsernameNotFoundException("User " + username + " was not found!");
+        }
+
+        if (account.getRoles() == null || account.getRoles().isEmpty()) {
+            throw new RuntimeException("User has no roles");
+        }
+
+        Collection<GrantedAuthority> authorities = account.getRoles().stream()
+                .map( role -> new SimpleGrantedAuthority(role.getName() )).collect(Collectors.toList());
+
+        return new User(
+                account.getUsername(),
+                account.getPassword(),
+                account.isEnabled(),
+                !account.isExpired(),
+                !account.isCredentialsExpired(),
+                !account.isLocked(),
+                authorities);
+    }
+}
